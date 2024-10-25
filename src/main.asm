e00f644ad53d13327b31f7218de6350b4d11591b
p34r1 <undeadgamer279@gmail.com>
2024-10-25 19:14:34 -0400

    write to file

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,13 @@
 section .data
   ;Constants
-  msg: db  "Hello, World!", 0xa ;our dear string
-  len: equ $ - msg              ;length of our dear string
+  msg: db  "Hello, World!", 0xa ; newline at end
+  len: equ $ - msg              ; get length
+  filename: db "output.txt", 0 ; Filename string, null-terminated
 
 
 section .bss
   ;variables
+  fileptr resq 1 ; Reserve space for the file descriptor (64 bits)
 
 
 section .text
@@ -13,13 +15,24 @@ section .text
 
   _start:
 
-  ; Write the string to stdout:
+  mov rdx, 0o644        ; mode: rw-r--r-- (octal 644)
+  mov rsi, 0x241        ; flags: O_CREAT | O_WRONLY | O_TRUNC (0x241)
+  mov rdi, filename
+  mov rax, 2            ; sys_open system call number is 2
+  syscall
+  mov [fileptr], rax  ; store the file descriptor in [fileptr]
+
 
   mov rdx,len ;message length
   mov rsi,msg ;message to write
-  mov rdi,1   ;file descriptor (stdout)
+  mov rdi,[fileptr] ;file descriptor (output.txt)
   mov rax,1   ;system call number (sys_write)
   syscall     ;call kernel
+
+  ; Close the file (sys_close)
+  mov rax, 3                    ; sys_close system call number is 3
+  mov rdi, [fileptr]            ; file descriptor to close
+  syscall
 
   ; Exit via the kernel:
 
